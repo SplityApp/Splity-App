52bb9dfbc48b3e198a8b2711d1fb92e1ef1dcd23
Igor Joński <jonskiigor@gmail.com>
2024-10-16 00:06:01 +0200

    Add Nunito font and create nunitoFontFamily

--- app/src/main/java/com/igorj/splity/ui/theme/Type.kt
+++ app/src/main/java/com/igorj/splity/ui/theme/Type.kt
@@ -2,9 +2,31 @@ package com.igorj.splity.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.igorj.splity.R
+
+val nunitoFontFamily = FontFamily(
+    Font(R.font.nunito_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.nunito_black_italic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.nunito_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.nunito_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.nunito_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.nunito_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.nunito_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.nunito_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.nunito_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.nunito_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.nunito_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.nunito_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.nunito_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.nunito_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.nunito_extra_light, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.nunito_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
