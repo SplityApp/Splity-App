f865950ccff390456a762505dc850588a52ead78
Igor Joński <jonskiigor@gmail.com>
2024-11-04 15:31:45 +0100

    Extract TpayModule configuration outside of Payment composable

--- app/src/main/java/com/igorj/splity/ui/composable/main/payment/Payment.kt
+++ app/src/main/java/com/igorj/splity/ui/composable/main/payment/Payment.kt
@@ -1,35 +1,49 @@
 package com.igorj.splity.ui.composable.main.payment
 
 import android.util.Log
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowForward
 import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentActivity
-import com.google.android.gms.wallet.Wallet
-import com.igorj.splity.BuildConfig
 import com.igorj.splity.model.payment.PaymentParticipant
-import com.tpay.sdk.api.models.*
-import com.tpay.sdk.api.models.merchant.Merchant
+import com.tpay.sdk.api.models.PayerContext
+import com.tpay.sdk.api.models.SheetOpenResult
 import com.tpay.sdk.api.models.payer.Payer
 import com.tpay.sdk.api.models.transaction.SingleTransaction
 import com.tpay.sdk.api.payment.Payment
 import com.tpay.sdk.api.payment.PaymentDelegate
-import com.tpay.sdk.api.providers.MerchantDetailsProvider
-import com.tpay.sdk.api.providers.SSLCertificatesProvider
 import com.tpay.sdk.api.screenless.Notifications
-import com.tpay.sdk.api.tpayModule.TpayModule
+import java.util.Currency
+import java.util.Locale
 
 @Composable
 fun Payment(
     amount: Double,
+    currency: Currency,
     payer: PaymentParticipant,
     receiver: PaymentParticipant,
 ) {
@@ -39,62 +53,6 @@ fun Payment(
     }
 
     var isLoading by remember { mutableStateOf(false) }
-
-    LaunchedEffect(true) {
-        TpayModule.configure(object : SSLCertificatesProvider {
-            override var apiConfiguration: CertificatePinningConfiguration =
-                CertificatePinningConfiguration(
-                    publicKeyHash = BuildConfig.TPAY_PUBLIC_KEY_HASH,
-                )
-        })
-
-        TpayModule.configure(
-            Merchant(
-                authorization = Merchant.Authorization(
-                    clientId = BuildConfig.TPAY_CLIENT_ID,
-                    clientSecret = BuildConfig.TPAY_CLIENT_SECRET
-                )
-            )
-        )
-
-        TpayModule.configure(object : MerchantDetailsProvider {
-            override fun merchantDisplayName(language: Language): String {
-                return when (language) {
-                    Language.PL -> "polish name"
-                    Language.EN -> "english name"
-                }
-            }
-
-            override fun merchantCity(language: Language): String {
-                return when (language) {
-                    Language.PL -> "Warszawa"
-                    Language.EN -> "Warsaw"
-                }
-            }
-
-            override fun regulationsLink(language: Language): String {
-                return when (language) {
-                    Language.PL -> "polish regulation url"
-                    Language.EN -> "english regulation url"
-                }
-            }
-        })
-
-        TpayModule.configure(Environment.SANDBOX)
-
-        TpayModule.configure(
-            preferredLanguage = Language.PL,
-            supportedLanguages = listOf(Language.PL, Language.EN)
-        )
-
-        TpayModule.configure(
-            paymentMethods = listOf(
-                PaymentMethod.Card,
-                PaymentMethod.Blik,
-                PaymentMethod.Pbl,
-            )
-        )
-    }
 
     Card(
         modifier = Modifier
@@ -114,7 +72,6 @@ fun Payment(
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                // From section
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.weight(1f)
@@ -135,14 +92,12 @@ fun Payment(
                     )
                 }
 
-                // Arrow
                 Icon(
                     imageVector = Icons.AutoMirrored.Outlined.ArrowForward,
                     contentDescription = "to",
                     modifier = Modifier.padding(horizontal = 8.dp)
                 )
 
-                // To section
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.weight(1f)
@@ -171,7 +126,7 @@ fun Payment(
                 )
             ) {
                 Text(
-                    text = String.format("%.2f", amount), //TODO: Tutaj należy dodać walutę, najlepiej na podstaiwe jakiegoś enuma który będzie zrobiony pryz okazji robienia taska z grupami
+                    text = String.format(Locale.getDefault(), "%.2f %s", amount, currency.symbol),
                     style = MaterialTheme.typography.headlineMedium,
                     modifier = Modifier
                         .fillMaxWidth()
@@ -203,7 +158,7 @@ fun Payment(
                                     email = payer.email,
                                     phone = payer.phoneNumber,
                                     address = Payer.Address(
-                                        city = "Warszawa",
+                                        city = "Warszawa",  // TODO: First empty, then after 1st completed payment get address from shared preferences
                                         postalCode = "00-000",
                                         countryCode = "PL",
                                         address = "ul. Testowa 1"
@@ -211,7 +166,7 @@ fun Payment(
                                 )
                             ),
                             notifications = Notifications(
-                                notificationEmail = "twoj@email.com",
+                                notificationEmail = "twoj@email.com",  // TODO: analyze if it's needed
                                 notificationUrl = "https://twojadomena.pl/notification"
                             )
                         ),
