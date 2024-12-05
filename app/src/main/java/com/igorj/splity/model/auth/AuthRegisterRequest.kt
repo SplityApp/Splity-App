ae77b4062422ebdfb4819493f87713d9abfa7632
Igor Joński <jonskiigor@gmail.com>
2024-12-05 23:22:19 +0100

    Attempt to fix register

--- app/src/main/java/com/igorj/splity/model/auth/AuthRegisterRequest.kt
+++ app/src/main/java/com/igorj/splity/model/auth/AuthRegisterRequest.kt
@@ -1,8 +1,11 @@
 package com.igorj.splity.model.auth
 
+import com.google.gson.annotations.SerializedName
+
 data class AuthRegisterRequest(
     val email: String,
     val password: String,
     val username: String,
+    @SerializedName("phone_number")
     val phoneNumber: String
 )
